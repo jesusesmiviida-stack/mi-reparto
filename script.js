16bc14a3950ff39416f5482a6eb1a97029fa1a90
jesusesmiviida-stack <jesusesmiviida@gmail.com>
2026-01-10 14:59:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,19 @@ document.addEventListener('DOMContentLoaded', () => {
         confetti({ particleCount: 100, spread: 70, origin: { y: 0.6 } });
     });
 });
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// Ejecutar una vez al cargar por si hay elementos visibles al inicio
+reveal();
